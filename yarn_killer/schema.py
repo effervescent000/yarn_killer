ea9a175cf00904b01c5d00752e8d0147144bf40e
effervescent000 <kislio01@gmail.com>
2022-01-18 17:26:28 -0500

    added colorway and fiber schema

--- yarn_killer/schema.py
+++ yarn_killer/schema.py
@@ -1,6 +1,22 @@
 from . import ma
 
 
+class FiberSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "yarn_id", "type", "amount")
+
+
+multi_fiber_schema = FiberSchema(many=True)
+
+
+class ColorwaySchema(ma.Schema):
+    class Meta:
+        fields = ("id", "yarn_id", "name", "color_broad", "color_medium", "color_specific")
+
+
+multi_colorway_schema = ColorwaySchema(many=True)
+
+
 class YarnSchema(ma.Schema):
     class Meta:
         fields = (
@@ -14,4 +30,9 @@ class YarnSchema(ma.Schema):
             "texture",
             "color_style",
             "discontinued",
+            "fibers",
+            "colorways",
         )
+
+    fibers = ma.Nested(multi_fiber_schema)
+    colorways = ma.Nested(multi_colorway_schema)
